63e60e0e5e2cac9693cb151333b41e9cc8d4dece
Andres Bustamante <abustamante@aitbol.com>
2020-05-06 10:50:27 -0400

    fix scroll style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,15 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.scrollStyle()
   }
 
+  scrollStyle() {
+    let progress = document.getElementById('progressbar');
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function () {
+      let progressHeight = (window.pageYOffset / totalHeight) * 100;
+      progress.style.height = progressHeight + '%'
+    }
+  }
 }
